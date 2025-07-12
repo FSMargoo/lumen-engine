a6f5bf80706e86f26a3e4bcbfc0b3b9d082b840f
GFigure <qiuzhengyu@siggraph.org>
2025-07-12 13:46:59 +0800

    Added IMGUI support

---
+++ lumen/inc/ImGUI/ImGUILayer.h
@@ -0,0 +1,10 @@
+﻿/**
+ * ImGUILayer.h:
+ *      @Descripiton    :   DESHERE
+ *      @Author         :   Margoo(qiuzhengyu@acm.org)
+ *      @Date           :   7/12/2025
+ */
+#ifndef IMGUILAYER_H
+#define IMGUILAYER_H
+
+#endif //IMGUILAYER_H
---
+++ lumen/src/ImGUI/ImGUILayer.cpp
@@ -0,0 +1,5 @@
+﻿/**
+ * ImGUILayer.cpp:
+ *      @Author         :   Margoo(qiuzhengyu@acm.org)
+ *      @Date           :   7/12/2025
+ */
